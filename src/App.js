ca8bda85bb8f37b354ff93797ad6b5d353a77563
xujj <939160741@qq.com>
2018-11-04 23:21:47 +0800

    事项备忘录

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+import 'normalize.css'
+import './reset.css'
+import * as localStore from './localStore'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: localStore.load('todoList') || []
+    }
+  }
+  render() {
+    let todos = this.state.todoList.filter((item) => !item.deleted)
+    .map((item,index)=>{
+      return ( // 为什么这里要加个括号？这是动手题3 🐸
+        <li key={index} >
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} 
+            onDelete={this.delete.bind(this)}/>
+        </li>
+      )
+    })
+    return (
+      <div className="App">
+          <h1>我的待办</h1>
+          <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo} 
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
+          </div>
+        <ol className="todoList">
+        {todos}
+        </ol>
+      </div>
+    )
+  }
+  componentDidUpdate(){
+    localStore.save('todoList', this.state.todoList)
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+  toggle(e, todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state) 
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  delete(event, todo){
+    todo.deleted = true
+    this.setState(this.state) 
+  }
+}
+
+export default App;
+
+let id = 0
+ function idMaker(){
+  id += 1
+  return id
+}
---
+++ src/TodoInput.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './TodoInput.css'
+
+export default class TodoInput extends Component {
+  render(){
+    return <input type="text" value={this.props.content}
+      className="TodoInput"
+      onChange={this.changeTitle.bind(this)}
+      onKeyPress={this.submit.bind(this)}/>
+  }
+  submit(e){
+    if (e.key === 'Enter') {
+      this.props.onSubmit(e)
+    }
+  }
+  changeTitle(e){
+    this.props.onChange(e)
+  }
+}
