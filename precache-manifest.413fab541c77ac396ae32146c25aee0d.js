f35088c173dfa7d39357ccd3a9f386d85694a12c
xujj <939160741@qq.com>
2018-11-05 00:07:13 +0800

    Updates

---
+++ precache-manifest.413fab541c77ac396ae32146c25aee0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce42f22a2df90539422c",
+    "url": "/TODOLIST-REACT/build/static/js/runtime~main.ce42f22a.js"
+  },
+  {
+    "revision": "f821a29429ed4ad84dfa",
+    "url": "/TODOLIST-REACT/build/static/js/main.f821a294.chunk.js"
+  },
+  {
+    "revision": "1b97c113fd7c7f34462d",
+    "url": "/TODOLIST-REACT/build/static/js/1.1b97c113.chunk.js"
+  },
+  {
+    "revision": "f821a29429ed4ad84dfa",
+    "url": "/TODOLIST-REACT/build/static/css/main.23ab23c2.chunk.css"
+  },
+  {
+    "revision": "1b97c113fd7c7f34462d",
+    "url": "/TODOLIST-REACT/build/static/css/1.903c0304.chunk.css"
+  },
+  {
+    "revision": "31d5d866493e70743979cbf650243623",
+    "url": "/TODOLIST-REACT/build/index.html"
+  }
+];
